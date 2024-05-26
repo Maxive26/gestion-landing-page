a45ea30aaaac6402bf61c6091a2c48f9533e2500
Maximiliano Vera <vera.maxi2002@gmail.com>
2024-05-25 21:07:29 -0300

    Fix

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -44,15 +44,15 @@ export default function ContactForm() {
           </div>
           <div className="relative">
             <label
-              for="message"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              htmlFor="message"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
               Mensaje
             </label>
             <textarea
               id="message"
               rows="4"
-              class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Dudas, consultas, mensaje..."
             ></textarea>
           </div>
